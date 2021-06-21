228d24892f2727fef6d60e148f15044c36ed0acd
loas <66953227+leohammer123@users.noreply.github.com>
2021-06-21 14:20:09 +0800

    Update alertsmn.py

--- sms message/alertsmn.py
+++ sms message/alertsmn.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACef6302e2af77181d03db93758a816e54"
+account_sid = ""
 # Your Auth Token from twilio.com/console
-auth_token  = "954e140ab41ee1c86753f00996ef9d4d"
+auth_token  = ""
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+886966309501", 
-    from_="+18326647285",
+    to="+", 
+    from_="+",
     body="Hello from Python!")
 
 print(message.sid)
