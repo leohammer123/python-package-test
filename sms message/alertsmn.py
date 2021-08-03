c0cd685653508b84cf666901110625075355efaa
loas <66953227+leohammer123@users.noreply.github.com>
2021-08-03 15:39:47 +0800

    123

--- sms message/alertsmn.py
+++ sms message/alertsmn.py
@@ -1,15 +1,16 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = ""
+account_sid = "ACef6302e2af77181d03db93758a816e54"
 # Your Auth Token from twilio.com/console
-auth_token  = ""
+auth_token  = "02706b1514f17376db723cb908bf00e0"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+", 
-    from_="+",
-    body="Hello from Python!")
+    to="+886966309501", 
+    from_="+18326647285",
+    body="牙牙牙牙牙牙")
 
 print(message.sid)
+#Iw376f5JQHShLzKyWZC-j1gxCMNQyK-WNarkR89s
